cd89d659fae7d60f471093c2409a7ec1bfdca23e
Ray <ray00000sina@gmail.com>
2019-03-02 18:18:19 +0800

    fixeds : 1.調整方塊消除分數。 2.方塊依等級掉落速度。 3.每個等級所需消除行數。

--- src/tetris/Config.java
+++ src/tetris/Config.java
@@ -7,19 +7,20 @@ package tetris;
  */
 public class Config {
 	private static Config sInstance = new Config();
-	
-	//遊戲版本
+
+	// 遊戲版本
 	private String mVersion = "1.4.3";
 
 	// 消除方塊行數可獲得的分數
-	private int [] mCleanLinesScore = { 0, 100, 300, 500, 800 };
-	
-	//方塊掉落速度(秒)
-	private float [] mBoxFallSpeed = { 2.0f, 1.5f, 1.0f, 0.5f, 0.4f, 0.3f, 0.2f, 0.1f, 0.08f };
+	private int[] mCleanLinesScore = { 0, 40, 100, 300, 1200 };
 
-	//每個等級升下一級所需的行數
-	private int mLevelUpLines = 10;
-	
+	// 方塊掉落速度(fps/秒)
+	private float[] mBoxFallSpeed = { 48, 43, 38, 33, 28, 23, 18, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2 };
+
+	// 每個等級升下一級所需的行數
+	private int[] mLevelUpLines = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 100, 100, 100, 100, 100, 100, 110, 120,
+			130 };
+
 	// 畫面比例
 	private double mScreenScale = 1.3;
 
@@ -28,14 +29,14 @@ public class Config {
 
 	// 重用音效緩存的筆數
 	private int mSoundCacheCount = 20;
-	
-	//下移1格時可獲得的分數
+
+	// 下移1格時可獲得的分數
 	private int mMoveDownScore = 5;
-	
-	//快速下移時可獲得的分數
+
+	// 快速下移時可獲得的分數
 	private int mQuickDownScore = 10;
-	
-	//可顯示將要的掉落方塊個數(畫面右側)
+
+	// 可顯示將要的掉落方塊個數(畫面右側)
 	private int mNextBoxs = 3;
 
 	private Config() {
@@ -44,12 +45,13 @@ public class Config {
 	public static Config get() {
 		return sInstance;
 	}
-	
+
 	/**
 	 * 取得遊戲版本
+	 * 
 	 * @return
 	 */
-	public String getVersion(){
+	public String getVersion() {
 		return mVersion;
 	}
 
@@ -94,50 +96,70 @@ public class Config {
 	public int getSoundCacheCount() {
 		return mSoundCacheCount;
 	}
-	
+
 	/**
 	 * 用等級取得方塊掉落速度
+	 * 
 	 * @param level
 	 * @return
 	 */
-	public float getBoxFallSpeed(int level){
-		if(level >= 0 && level < mBoxFallSpeed.length){
-			return mBoxFallSpeed[level];
+	public float getBoxFallSpeed(int level) {
+		if (level >= 0 && level < mBoxFallSpeed.length) {
+			return mBoxFallSpeed[level] / 60.0f;
 		}
-		return mBoxFallSpeed[mBoxFallSpeed.length - 1];
+		return mBoxFallSpeed[mBoxFallSpeed.length - 1] / 60.0f;
 	}
-	
+
 	/**
 	 * 已消除的方塊行數轉換為對應的等級
 	 * 
 	 * @param lines
 	 * @return
 	 */
-	public int linesConvertLevel(int lines){
-		return (lines / mLevelUpLines);
+	public int linesConvertLevel(int lines) {
+		int level = 0;
+
+		for (int n : mLevelUpLines) {
+			lines -= n;
+
+			if (lines < 0) {
+				break;
+			}
+			level++;
+		}
+
+		int maxLines = mLevelUpLines[mLevelUpLines.length - 1];
+
+		if (lines > maxLines) {
+			level += (lines / maxLines);
+		}
+		return level;
 	}
-	
+
 	/**
 	 * 取得下移1格時可獲得的分數
+	 * 
 	 * @return
 	 */
-	public int getMoveDownScore(){
+	public int getMoveDownScore() {
 		return mMoveDownScore;
 	}
-	
+
 	/**
 	 * 取得快速下速方塊可獲得的分數
+	 * 
 	 * @return
 	 */
-	public int getQuickDownScore(){
+	public int getQuickDownScore() {
 		return mQuickDownScore;
 	}
-	
+
 	/**
 	 * 可顯示將要的掉落方塊個數(畫面右側)
+	 * 
 	 * @return
 	 */
-	public int getNextBoxs(){
+	public int getNextBoxs() {
 		return mNextBoxs;
 	}
 }
